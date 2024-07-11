6b22275de8eb81d7c21fab9aa10d6e268b38c83d
Newton Wakhungu <wakhungu22newton@gmail.com>
2024-07-11 12:51:02 +0300

    chore: add currency on total

--- src/components/front-end/Cart.tsx
+++ src/components/front-end/Cart.tsx
@@ -37,7 +37,7 @@ const Cart: React.FC<PropsType> = ({ setShowCart }) => {
                 </div>
                 <div className='flex justify-between items-center font-medium text-xl py-4'>
                     <p>Total:</p>
-                    <p>${getTotal()}.00</p>
+                    <p>Ksh{getTotal()}.00</p>
                 </div>
                 <button className='bg-black text-white text-center w-full rounded-3xl py-2 hover:bg-accent mb-4 mt-4'>
                     View cart
