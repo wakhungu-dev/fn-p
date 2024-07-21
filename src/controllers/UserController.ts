245d562ef07d6cd3b9f0b972b4f323c09ec2e203
Newton Wakhungu <wakhungu22newton@gmail.com>
2024-07-21 11:07:06 +0300

    chore: add nextauth secret

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -4,7 +4,7 @@ import { mongoDbConnection } from "@/libs/mongoDb";
 class UserController {
   db: typeof User;
   constructor() {
-    this.dbInit();
+    // this.dbInit();
     this.db = User;
   }
   async dbInit() {
@@ -12,6 +12,7 @@ class UserController {
   }
   async getUsers() {
     try {
+      await this.dbInit();
       const users = await this.db.find();
       return users;
     } catch (error) {
@@ -20,6 +21,7 @@ class UserController {
   }
   async getUserById(id: string) {
     try {
+      await this.dbInit();
       const user = await this.db.findById(id);
       return user;
     } catch (error) {
@@ -28,6 +30,7 @@ class UserController {
   }
   async createUser(data: any) {
     try {
+      await this.dbInit();
       const newUser = new this.db(data);
       const user = await newUser.save();
       return user;
@@ -37,6 +40,7 @@ class UserController {
   }
   async updateUser(id: string, data: any) {
     try {
+      await this.dbInit();
       const user = await this.db.findByIdAndUpdate(id, data, { new: true });
       return user;
     } catch (error) {
@@ -45,6 +49,7 @@ class UserController {
   }
   async deleteUser(id: string) {
     try {
+      await this.dbInit();
       const user = await this.db.findByIdAndDelete(id);
       return user;
     } catch (error) {
@@ -54,6 +59,7 @@ class UserController {
 
   async getUserByEmail(email: string) {
     try {
+      await this.dbInit();
       const user = await this.db.findOne({ email });
       return user;
     } catch (error) {
