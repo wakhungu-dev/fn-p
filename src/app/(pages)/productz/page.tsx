b2760cf326c7aea2c87ea320a9feef6ccbff0391
Newton Wakhungu <wakhungu22newton@gmail.com>
2024-07-24 12:24:44 +0300

    feat : add productz folder ,update the hero section to incluide shop now functionality

---
+++ src/app/(pages)/productz/page.tsx
@@ -0,0 +1,14 @@
+// pages/products.tsx
+import TrendingProduct from "@/components/front-end/TrendingProduct";
+import React from "react";
+
+const Products = () => {
+  return (
+    <div className="container mx-auto py-8">
+      <h1 className="text-4xl font-bold mb-8">Our Products</h1>
+      <TrendingProduct  />
+    </div>
+  );
+};
+
+export default Products;
